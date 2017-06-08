02fe0477ccb00c15011e677753d5bca5abe31e75
erikvw <ew2789@gmail.com>
2017-06-08 10:17:36 +0200

    fix viait tracking app error for tests

--- bcpp_clinic_subject/apps.py
+++ bcpp_clinic_subject/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig as DjangoApponfig
 
+from edc_visit_tracking.apps import AppConfig as BaseEdcVisitTrackingAppConfig
+
 
 class AppConfig(DjangoApponfig):
     name = 'bcpp_clinic_subject'
@@ -11,3 +13,8 @@ class AppConfig(DjangoApponfig):
     admin_site_name = 'bcpp_clinic_subject_admin'
     eligibility_age_adult_lower = 16
     eligibility_age_adult_upper = 64
+
+
+class EdcVisitTrackingAppConfig(BaseEdcVisitTrackingAppConfig):
+    visit_models = {
+        'bcpp_clinic_subject': ('subject_visit', 'bcpp_clinic_subject.subjectvisit')}
