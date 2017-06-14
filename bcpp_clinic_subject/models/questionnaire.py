58c01c69045410e0c11e9f59970b5c39c9a75996
aseokana <aseokana@bhp.org.bw>
2017-06-14 16:40:50 +0200

    adding some fields

--- bcpp_clinic_subject/models/questionnaire.py
+++ bcpp_clinic_subject/models/questionnaire.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 from edc_base.model_fields.custom_fields import OtherCharField
-from edc_constants.choices import YES_NO_DWTA
+from edc_constants.choices import YES_NO_DWTA, POS_NEG_ONLY, YES_NO
 
 from ..choices import REGISTRATION_TYPES
 from ..models.crf_model_mixin import CrfModelMixin
@@ -20,11 +20,31 @@ class Questionnaire(CrfModelMixin):
 
     registration_type_other = OtherCharField()
 
+    know_hiv_status = models.CharField(
+        verbose_name="Do you know you HIV status?",
+        max_length=25,
+        choices=YES_NO_DWTA,
+    )
+
+    current_hiv_status = models.CharField(
+        verbose_name="What is your current HIV status?",
+        max_length=25,
+        choices=POS_NEG_ONLY,
+        null=True,
+        blank=True,
+    )
+
     on_arv = models.CharField(
         verbose_name="Are you currently taking antiretroviral therapy (ARVs)?",
         max_length=25,
         choices=YES_NO_DWTA,
         help_text="",
+    )
+
+    arv_evidence = models.CharField(
+        verbose_name="Do you have evidence of the antiretroviral therapy ARVs you're taking?",
+        max_length=25,
+        choices=YES_NO
     )
 
     knows_last_cd4 = models.CharField(
