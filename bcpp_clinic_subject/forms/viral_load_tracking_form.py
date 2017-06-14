43f9d25754f802a5267c3bca2efbae2ac58460b2
aseokana <aseokana@bhp.org.bw>
2017-06-14 16:33:26 +0200

    adding validations

--- bcpp_clinic_subject/forms/viral_load_tracking_form.py
+++ bcpp_clinic_subject/forms/viral_load_tracking_form.py
@@ -1,23 +1,15 @@
-from django import forms
-
-from edc_constants.constants import YES
+from bcpp_clinic_validations.form_validators import ViralLoadTrackingFormValidator
 
 from ..models import ViralLoadTracking
 from .modelform_mixin import SubjectModelFormMixin
 
 
-class ViralLoadTrackingForm(SubjectModelFormMixin):
+class ViralLoadTrackingForm (SubjectModelFormMixin):
 
     def clean(self):
         cleaned_data = super().clean()
-        if cleaned_data.get('is_drawn') == YES and not cleaned_data.get('clinician_initials'):
-            raise forms.ValidationError(
-                'If sample was drawn, please provide the Clinician\'s initials')
-
-        if cleaned_data.get('is_drawn') == YES and not cleaned_data.get('drawn_datetime'):
-            raise forms.ValidationError(
-                'If sample was drawn, please provide the date/time sample drawn initials')
-
+        cleaned_data = ViralLoadTrackingFormValidator(
+            cleaned_data=cleaned_data).clean()
         return cleaned_data
 
     class Meta:
