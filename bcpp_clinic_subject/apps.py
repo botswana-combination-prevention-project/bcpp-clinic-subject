97e1128688ad3b800160bd3d95a6c04a3ac3ae26
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-06-15 12:29:41 +0200

    fix admin urls

--- bcpp_clinic_subject/apps.py
+++ bcpp_clinic_subject/apps.py
@@ -5,7 +5,7 @@ class AppConfig(DjangoApponfig):
     name = 'bcpp_clinic_subject'
     listboard_template_name = 'bcpp_clinic_subject/listboard.html'
     dashboard_template_name = 'bcpp_clinic_subject/dashboard.html'
-    base_template_name = 'edc_base/base.html'
+    base_template_name = 'bcpp_clinic/base.html'
     listboard_url_name = 'bcpp_clinic_subject:listboard_url'
     dashboard_url_name = 'bcpp_clinic_subject:dashboard_url'
     admin_site_name = 'bcpp_clinic_subject_admin'
