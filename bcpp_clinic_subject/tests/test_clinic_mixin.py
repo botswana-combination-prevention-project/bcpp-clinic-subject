77995fdaec8ba5d625ece7a20605bb530ed37381
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-07-05 16:10:54 +0200

    remove mappers.py

--- bcpp_clinic_subject/tests/test_clinic_mixin.py
+++ bcpp_clinic_subject/tests/test_clinic_mixin.py
@@ -6,11 +6,32 @@ from edc_base.utils import get_utcnow
 from edc_constants.constants import YES, MALE, POS, NOT_APPLICABLE
 from edc_visit_tracking.constants import SCHEDULED
 
+from edc_map.mapper import Mapper
+from edc_map.site_mappers import site_mappers
+
 from bcpp_clinic_screening.constants import ABLE_TO_PARTICIPATE
 from bcpp_clinic_screening.models import SubjectEligibility
 
 from ..models.appointment import Appointment
 from ..models.subject_visit import SubjectVisit
+
+
+class TestPlotMapper(Mapper):
+
+    map_area = 'test_community'
+    map_code = '01'
+    pair = 0
+    landmarks = ()
+    center_lat = -24.557709
+    center_lon = 25.807963
+    clinic_lat = -24.645541
+    clinic_lon = 25.912407
+    radius = 100.5
+    location_boundary = ()
+    intervention = True
+
+
+site_mappers.register(TestPlotMapper)
 
 
 class TestClinicMixin:
