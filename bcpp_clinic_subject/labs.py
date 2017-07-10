456fd9170230e33b6d35388d4ccbc98988ae14fb
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-07-10 09:35:12 +0200

    update protocol number on subject requisition

--- bcpp_clinic_subject/labs.py
+++ bcpp_clinic_subject/labs.py
@@ -10,6 +10,7 @@ lab_profile = LabProfile(
 wb = AliquotType(name='Whole Blood', alpha_code='WB', numeric_code='02')
 bc = AliquotType(name='Buffy Coat', alpha_code='BC', numeric_code='16')
 pl = AliquotType(name='Plasma', alpha_code='PL', numeric_code='32')
+
 wb.add_derivatives(pl, bc)
 
 
