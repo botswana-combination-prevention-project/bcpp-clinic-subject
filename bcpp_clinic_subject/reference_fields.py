b3565cfa4d75b1535fdbf0ae9e46b39e9c3056db
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-07-25 13:16:28 +0200

    Fix referrence config import errors

--- bcpp_clinic_subject/reference_fields.py
+++ bcpp_clinic_subject/reference_fields.py
@@ -1,23 +1,23 @@
-from edc_reference.reference_model_config import ReferenceModelConfig
-from edc_reference.site import site_reference_fields
+from edc_reference.site import ReferenceModelConfig
+from edc_reference.site import site_reference_configs
 
 
 reference = ReferenceModelConfig(
     model='bcpp_clinic_subject.questionnaire',
     fields=['registration_type'])
-site_reference_fields.register(reference)
+site_reference_configs.register(reference)
 
 reference = ReferenceModelConfig(
     model='bcpp_clinic_subject.viralloadtracking',
     fields=['is_drawn'])
-site_reference_fields.register(reference)
+site_reference_configs.register(reference)
 
 reference = ReferenceModelConfig(
     model='bcpp_clinic_subject.vlresult',
     fields=['report_datetime'])
-site_reference_fields.register(reference)
+site_reference_configs.register(reference)
 
 reference = ReferenceModelConfig(
     model='bcpp_clinic_subject.subjectrequisition',
     fields=['report_datetime'])
-site_reference_fields.register(reference)
+site_reference_configs.register(reference)
