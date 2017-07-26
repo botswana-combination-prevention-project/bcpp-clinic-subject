e98974320cd8d1ec913aee1d20425696e5c5655a
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-07-26 18:26:58 +0200

    update repo name in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='clinic-subject',
+    name='bcpp-clinic-subject',
     version='0.1dev0',
     author=u'Coulson Thabo Kgathi',
     author_email='ckgathi@gmail.com',
