273a9f7a26e5c8e5d0f0e8dc9960f315fe5c6e80
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-08-21 11:35:58 +0200

    update urls

--- bcpp_clinic_subject/urls.py
+++ bcpp_clinic_subject/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from .admin_site import bcpp_clinic_subject_admin
 
+app_name = 'bcpp_clinic_subject'
+
 urlpatterns = [
     url(r'^admin/', bcpp_clinic_subject_admin.urls),
 ]
